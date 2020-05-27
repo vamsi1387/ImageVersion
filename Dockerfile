456960003c1bbdd549cc7d6c3a92b07902ca7853
Vamsi <vamsikrishna_a01@infosys.com>
2020-05-27 05:47:05 +0000

    Add existing file

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM ubuntu
+
+COPY helloworld.sh /opt/app/
+
+WORKDIR /opt/app/
+
+CMD sh helloworld.sh
---
+++ helloworld.sh
@@ -0,0 +1,4 @@
+echo "hello world"
+echo "adding a new line as part of version 1"
+echo "adding a new line for release script"
+
