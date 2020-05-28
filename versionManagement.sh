25f7402b4fc865195fe25deec358c91d5554de95
Vamsi <vamsikrishna_a01@infosys.com>
2020-05-28 04:58:16 +0000

    version 0.1.2

--- versionManagement.sh
+++ versionManagement.sh
@@ -46,9 +46,7 @@ then
 	docker push $USERNAME/$IMAGE:$NEW_VERION
 
 else
-        #addVersion //calling addVersion method for incrementing the image version
-        #VERSION=`cat VERSION`
-        OLD_VERSION=`docker images | grep $IMAGE | grep -v latest | head -1 | awk '{print $2}'`
+        OLD_VERSION=`docker images | grep -w $IMAGE | grep -v latest | head -1 | awk '{print $2}'`
         NEW_VERION=`addVersion $OLD_VERSION`
         echo "New_Version: $NEW_VERION"
 	docker commit $containerid $USERNAME/$IMAGE:$NEW_VERSION
