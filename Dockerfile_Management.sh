8c01c70d250dd977c64a8add3e61c73289550af1
Vamsi <vamsikrishna_a01@infosys.com>
2020-06-04 19:22:57 +0000

    testing docker file versioining

--- Dockerfile_Management.sh
+++ Dockerfile_Management.sh
@@ -1,22 +1,73 @@
 
+#addVersion()
+#{
+#  val=$1
+#  dotCount=`echo $1 | tr -cd '.'| wc -c`
+  #if there is no version tag yet, let's start at 0.0.0
+#  if [ -z "$val" ]; then
+     #No existing version, starting at 0.0.0
+#     echo "0.0.0"
+#     exit    
+#  fi
+#  if [ $dotCount -eq 0 ]
+#  then
+#      val=`expr $val + 1`
+#  else
+#      val=`echo $val | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}'`
+#  fi
+#  echo $val 
+#}
+
 addVersion()
 {
   val=$1
+  if [ -z "$2" ]; then
+    type=PATCH
+  else
+    type=`echo $2 | tr [a-z] [A-Z]`
+  fi
   dotCount=`echo $1 | tr -cd '.'| wc -c`
   #if there is no version tag yet, let's start at 0.0.0
-  if [ -z "$val" ]; then
+  if [ -z "$val" ] || [ "$val" == "<none>" ] ; then
      #No existing version, starting at 0.0.0
      echo "0.0.0"
-     exit    
+     exit
   fi
   if [ $dotCount -eq 0 ]
   then
       val=`expr $val + 1`
   else
-      val=`echo $val | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}'`
+      if [ $dotCount -eq 2 ]; then
+           majorVersion=`echo $1 | cut -d '.' -f1`
+           minorVersion=`echo $1 | cut -d '.' -f2`
+           patchVersion=`echo $1 | cut -d '.' -f3`
+           if [ "$type" == PATCH ]; then
+                patchVersion=`expr $patchVersion + 1`
+           elif [ "$type" == MINOR ]; then
+                minorVersion=`expr $minorVersion + 1`
+           else
+                majorVersion=`expr $majorVersion + 1`
+                minorVersion=0
+                patchVersion=0
+           fi
+           val=$majorVersion"."$minorVersion"."$patchVersion
+      elif [ $dotCount -eq 1 ]; then
+          majorVersion=`echo $1 | cut -d '.' -f1`
+          patchVersion=`echo $1 | cut -d '.' -f2`
+           if [ "$type" == PATCH ]; then
+                patchVersion=`expr $patchVersion + 1`
+           elif [ "$type" == MINOR ]; then
+                minorVersion=`expr $minorVersion + 1`
+           else
+                majorVersion=`expr $majorVersion + 1`
+                patchVersion=0
+           fi
+           val=$majorVersion"."$patchVersion
+      fi
   fi
-  echo $val 
+  echo $val
 }
+
 
 USERNAME=vamsi1387
 IMAGE=helloworld
