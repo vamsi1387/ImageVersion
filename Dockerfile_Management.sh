6a46a1bc24d1668120e66d9a66d1ea0ece93330a
Vamsi <vamsikrishna_a01@infosys.com>
2020-06-10 05:59:15 +0000

    testing

--- Dockerfile_Management.sh
+++ Dockerfile_Management.sh
@@ -1,23 +1,4 @@
 
-#addVersion()
-#{
-#  val=$1
-#  dotCount=`echo $1 | tr -cd '.'| wc -c`
-  #if there is no version tag yet, let's start at 0.0.0
-#  if [ -z "$val" ]; then
-     #No existing version, starting at 0.0.0
-#     echo "0.0.0"
-#     exit    
-#  fi
-#  if [ $dotCount -eq 0 ]
-#  then
-#      val=`expr $val + 1`
-#  else
-#      val=`echo $val | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}'`
-#  fi
-#  echo $val 
-#}
-
 addVersion()
 {
   val=$1
@@ -79,6 +60,7 @@ echo "New_Version: $NEW_VERION"
 docker build -t $USERNAME/$IMAGE:latest .
 # tag it
 git tag -a "$NEW_VERION" -m "version $NEW_VERION"
+git push
 git push --tags
 docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$NEW_VERION
 # push it
